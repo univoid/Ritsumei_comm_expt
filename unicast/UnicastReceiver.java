df078e9733be059eeab7c336d6726372e043d2fc
univoid <zyhlxw@gmail.com>
2014-10-13 23:59:06 +0900

    fixed files

--- unicast/UnicastReceiver.java
+++ unicast/UnicastReceiver.java
@@ -1,79 +1,54 @@
-/**
- *
- * UnicastReceiver.java
- *
- */
-import java.net.*;
+/*
+*
+* java UnicastReceiver filename
+*
+*/
 import java.io.*;
+import java.net.*;
 
-public class UnicastReceiver{
-   static final int echo_PORT = 50000;
-   static final int BUFSIZE = 1792;
-   public static void main(String[] args) throws IOException{
-      int port = echo_PORT;
-      int cliport;
-      int len;
-      int i;
-      DatagramSocket sock = null;  /
-      byte[] buf = new byte[BUFSIZE];
-      byte[] wbuf = new byte[BUFSIZE];
-      String recvdata;
-      try{
-         sock = new DatagramSocket(port);
-         DatagramPacket recvpacket = new DatagramPacket(buf,BUFSIZE);
-         DatagramPacket sendpacket = new DatagramPacket(buf,BUFSIZE);
-         System.out.println("Connected to echo server");
-         while(true){
-            sock.receive(recvpacket);
-            recvdata = new String(recvpacket.getData(),0,recvpacket.getLength());
-            recvdata = recvdata.trim();
-            outputStream out = new FileOutputStream("..");
-            BufferedOutputStream bout = new BufferedOutputStream(out);
-            //wbuf = recvdata.getBytes();
-            i = 0;
-            //len = wbuf.length;
-            buf = new byte[BUFSIZE];
-	    pkg = new DatagramPacket(buf, buf.length);
-
-            byte[] messagebuf = new byte[1024];
-            messagebuf = "ok".getBytes();
-            messagepkg = new DatagramPacket(messagebuf, messagebuf.length,
-                    new InetSocketAddress(ip, clientPort));
-            // 循环接收包，每接到一个包后回给对方一个确认信息，对方才发下一个包(避免丢包和乱序)，直到收到一个结束包后跳出循环，结束文件传输，关闭流
-            while (true) {
-                receive.receive(pkg);
-                if (new String(pkg.getData(), 0, pkg.getLength()).equals("end")) {
-                    System.out.println("文件接收完毕");
-                    bos.close();
-                    receive.close();
+public class UnicastReceiver {
+    static final int uni_PORT = 55555;  
+    static final int BUFSIZE = 1792;
+    public static void main(String[] args) {
+	FileOutputStream out = null;		//file
+        BufferedOutputStream bout = null;	//bin file
+        DatagramSocket serverSocket = null;	//socket
+        DatagramPacket getPacket = null;	//get packet
+        DatagramPacket sendPacket = null;	//send packet
+	InetAddress ip = null;			//ip
+	
+        try {
+	    System.out.println("Server start.");
+	    int port  = uni_PORT;		          
+	    //ip = InetAddress.getLocalHost();
+	    //System.out.println(ip);
+            serverSocket = new DatagramSocket(port);
+            out = new FileOutputStream(args[0]);
+	    bout = new BufferedOutputStream(out);
+            byte[] buf = new byte[BUFSIZE];
+            int i=0;
+	    //get file
+            while(true){
+                getPacket = new DatagramPacket(buf, 0, buf.length);
+                serverSocket.receive(getPacket);
+                i = getPacket.getLength();
+                bout.write(buf,0,i);
+                if(i<BUFSIZE){
                     break;
                 }
-                receive.send(messagepkg);
-                System.out.println(new String(messagepkg.getData()));
-                bos.write(pkg.getData(), 0, pkg.getLength());
-                bos.flush();
             }
-            bos.close();
-            receive.close();
- //           while(wbuf[i] != 0x0d){  //rÌCRo
- //              buf[i] = wbuf[i];  //f[^Rs[
- //              i++;
- //              if(i >=len){
- //                 break;
- //              }
- //           }
- //           recvdata = new String(buf,0,i);  //oCgzñš¶ñÏ·
- //           System.out.println(recvdata);  //æÊoÍ
- //           InetAddress ipadr = recvpacket.getAddress();
- //           cliport = recvpacket.getPort();
- //           buf = recvdata.getBytes();
- //           len = buf.length;
- //           sendpacket = new DatagramPacket(buf,len,ipadr,cliport);
- //           sock.send(sendpacket);  //f[^M
- //           recvpacket.setLength(BUFSIZE);  //f[^OpPbg·Zbg
-         }
-      }catch(SocketException e){
-         e.printStackTrace();
-      }
-   }
+	    System.out.println("Get done.");
+  	    //feedback
+            SocketAddress sendAddress = getPacket.getSocketAddress();
+            String feedback = "received successfully!";
+            byte[] backBuf = feedback.getBytes();
+            sendPacket = new DatagramPacket(backBuf,  backBuf.length, sendAddress);
+            serverSocket.send(sendPacket);
+
+	    bout.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- unicast/UnicastSender.java
+++ unicast/UnicastSender.java
@@ -1,58 +1,52 @@
-/**
-
- *
- * 用法例
- * >java UnicastSender 192.168.1.1 data.txt
- */
-import java.net.*;
+/*
+*
+* java UnicastSender IP filename
+*
+*/
 import java.io.*;
+import java.net.*;
 
-public class UnicastSender{
-   static final int echo_PORT = 7;  //エコーポート番号
-   static final int BUFSIZE = 1792;  //バッファサイズ
+public class UnicastSender {
+    static final int uni_PORT = 55555;  
+    static final int BUFSIZE = 1792;
+    public static void main(String[] args) {
+        FileInputStream in = null;              //file
+	BufferedInputStream bin= null;		//bin file
+        DatagramSocket sendSocket= null;        //socket
+        DatagramPacket sendPacket =null;        //send packet
+        DatagramPacket getPacket = null;        //get packet
+        InetAddress ip = null;                  //ip
 
-   public static void main(String[] args) throws IOException{
-      int port = echo_PORT;  //ポート番号
-      //byte[] buf = new byte[BUFSIZE];  //送受信バッファ
-      String fileName;  //file name
-      DatagramSocket sock = null;  //データグラムソケット宣言，初期化
-      if(args.length != 1){
-         throw new IllegalArgumentException("usage: >java  UnicastSender＜IP_Address＞");
-      }
-      while(true){
-         try{
-            InetAddress adr = InetAddress.getByName(args[0]);  //サーバIPアドレス
-            //System.out.print("cli>");  //プロンプト出力
-            fileName = args[1];  //キー入力
-
-	        //input begin
-	        InputStream in = new FileInputStream(fileName);  //入力ファイルを開く
-            //BufferedInputStreamオブジェクトの生成
-            BufferedInputStream bin = new BufferedInputStream(in);
-            byte buf[] = new byte[BUFSIZE];
-            int c;
-	        sock =new DatagramSocket();
-            //読み込みデータがなくなるまで読み込み
-            while((c = bin.read(buf)) != -1){
-               DatagramPacket packet = new DatagramPacket(buf,c,adr,port);
-               sock.send(packet);  //入力データ送信
-               sock.receive(packet);  //エコーデータ受信
+        try {
+            sendSocket = new DatagramSocket();
+            int port = uni_PORT;
+            ip = InetAddress.getByName(args[0]);
+            in = new FileInputStream(args[1]);
+	    bin = new BufferedInputStream(in);
+            byte[] buf =new byte[BUFSIZE];
+            int i;
+	    //send file
+            while((i = bin.read(buf, 0, BUFSIZE) )==BUFSIZE){
+                sendPacket = new DatagramPacket(buf, buf.length, ip,  port);
+                sendSocket.send(sendPacket);
+                Thread.sleep(5);
             }
-	        //input end
-	        //
-            //sock = new DatagramSocket();
-            //DatagramPacket packet = new DatagramPacket(buf,buf.length,adr,port);
-            //sock.send(packet);  //入力データ送信
-            //buf = new byte[BUFSIZE];
-            //packet = new DatagramPacket(buf,buf.length);
-            //sock.receive(packet);  //エコーデータ受信
-            //String data = new String(packet.getData(),0,packet.getLength());
-            //System.out.println(data);  //画面出力
-	        bin.close();  //入力ファイルを閉じる
-            sock.close();
-         }catch(SocketException e){
+            sendPacket = new DatagramPacket(buf, 0, i, ip, port);
+            sendSocket.send(sendPacket);
+            Thread.sleep(5);
+            System.out.println("Send done.");
+	    //get feedback message
+            byte[] getBuf = new byte[BUFSIZE];
+            getPacket = new DatagramPacket(getBuf, getBuf.length);
+            sendSocket.receive(getPacket);
+            String backMes = new String(getBuf, 0, getPacket.getLength());
+            System.out.println("feefBack:"+backMes);
+
+	    bin.close();
+        } catch (Exception e) {
             e.printStackTrace();
-         }
-      }
-   }
+        }
+
+    }
+
 }
